eac19cf8854488fcba690629b9bff69ab6c39d62
Riccardo <riccardo.manzoni@cern.ch>
2014-02-04 00:25:44 +0100

    easier and more flexible code. It produces a root file with all the relevant histos. Plotting needs to be done afterwards

--- working/PATreader.py
+++ working/PATreader.py
@@ -1,24 +1,14 @@
 import ROOT
-import sys
 import math
-import style
-from files import *
-from config import *
-from utils import *
-from copy import deepcopy as dc
+from DataFormats.FWLite import Events, Handle
 
 ROOT.TH1.SetDefaultSumw2()
 
 class PATreader() :
 
-  def __init__(self, basic_histos, track_histos) :
+  def __init__(self, files, basic_histos, track_histos) :
 
-    self.events = Events (
-    myFiles
-    # ['/afs/cern.ch/work/m/manzoni/TauHLT/CMSSW_5_3_14_patch2_trg/src/TriggerStudies/Tau/test/patTuple.root',] 
-    # ['/afs/cern.ch/work/m/manzoni/TauHLT/CMSSW_5_3_14_patch2_trg/src/TriggerStudies/Tau/test/patTuple_loosenIter0Jet_v2.root',] 
-    # ['/afs/cern.ch/work/m/manzoni/TauHLT/CMSSW_5_3_14_patch2_trg/src/TriggerStudies/Tau/test/patTuple_originalIter0Jet_v2.root',] 
-    )
+    self.events = Events ( files )
     self.declareHandles()
     self.allEvents         = 0
     self.failinRecoHLT_pix = 0
@@ -30,17 +20,18 @@ class PATreader() :
     self.basic_histos      = basic_histos
     self.track_histos      = track_histos
     
-  def looper(self, maxEvents=-1, pickSingleEvent=-1, pickSomeEvents=[], verbose=False) :
+  def looper(self, maxEvents=-1, pickEvents=[], verbose=False) :
         
     for loopId, event in enumerate(self.events):
-  
+      
+      self.event = event
+      
       if maxEvents > 0 : totEvents = maxEvents
       else             : totEvents = self.events.size()
       if maxEvents > 0 and loopId > maxEvents: break
-      if loopId%(self.events.size()/20)==0 : print '=====> \t', round( float(loopId)/float(totEvents)*100., 0) ,'%'
+      if loopId%(totEvents/20)==0 : print '=====> \t', round( float(loopId)/float(totEvents)*100., 0) ,'%'
       
-      if event.eventAuxiliary().event() not in pickSomeEvents and len(pickSomeEvents)>0 : continue
-      if event.eventAuxiliary().event() != pickSingleEvent  and pickSingleEvent > 0     : continue
+      if event.eventAuxiliary().event() not in pickEvents and len(pickEvents)>0 : continue
 
       self.produceCollections(event, self.handles)
 
@@ -55,65 +46,66 @@ class PATreader() :
       if len(off_tau) != 1                                 : continue  
       if abs(off_vtx[0].z()-off_tau[0].vertex().z()) > 0.2 : continue  
       if off_tau[0].charge() * off_mu[0].charge() >= 0     : continue
-      if deltaR(off_tau[0], off_mu[0]) < 0.5               : continue  
+      if self.deltaR(off_tau[0], off_mu[0]) < 0.5          : continue  
 
       self.allEvents += 1
       
-      event.tau = off_tau[0]
-      event.mu  = off_mu [0]
+      tau = off_tau[0]
+      mu  = off_mu [0]
 
-      event.onl_tau_mu_vtx  = self.best_matching([event.tau], onlTausMuVtx , dR=0.5).values()[0]
-      event.onl_tau_pix_vtx = self.best_matching([event.tau], onlTausPixVtx, dR=0.5).values()[0]
+      onl_tau_mu_vtx  = self.best_matching([tau], onlTausMuVtx , dR=0.5).values()[0]
+      onl_tau_pix_vtx = self.best_matching([tau], onlTausPixVtx, dR=0.5).values()[0]
       
-      event.tau.onlMu  = event.onl_tau_mu_vtx
-      event.tau.onlPix = event.onl_tau_pix_vtx
+      tau.onlMu               = onl_tau_mu_vtx
+      tau.onlPix              = onl_tau_pix_vtx
+      tau.genDM               = self.genDecayMode(self.getGenJetConstituents(tau))
+      tau.offlineLeadingTrack = self.checkLeadingTrack(tau)
+      if tau.offlineLeadingTrack is False : continue
+      tau.onlineLeadingTrack  = self.best_matching([tau.offlineLeadingTrack], [cand for cand in onlPFcandidates if cand.charge()!=0], dR=0.1).values()[0]
       
-      tau.genDM = self.genDecayMode(self.getGenJetConstituents(event.tau))
-
       if verbose :
         print '\nevent', event.eventAuxiliary().event()   
         print 'tau pt', tau.pt(), '\teta', tau.eta(), '\tphi', tau.phi(), '\tcharge', tau.charge(), '\trecoDM', tau.decayMode(), '\tgenDM', tau.genDM   
 
-      passMu  = True
-      passPix = True
+      self.fillBasicHistos(self.basic_histos,'offTaus',tau)
 
-      self.fillBasicHistos(self.basic_histos,'offTaus',event.tau)
-
-      if not event.onl_tau_mu_vtx :
+      ## check whether Tau is reconstructed online - MuVtx
+      if not onl_tau_mu_vtx :
         self.failinRecoHLT_mu  += 1
-        passMu = False
-        self.fillBasicHistos(self.basic_histos,'onlTauMu',event.tau)
-
-      if not event.onl_tau_pix_vtx :
-        self.failinRecoHLT_pix += 1  
-        passPix = False
-        self.fillBasicHistos(self.basic_histos,'onlTauPix',event.tau)
-
-      if passMu :
-        if event.onl_tau_mu_vtx.tauID('decayModeFinding') < 0.5  :
+      else :
+        self.fillBasicHistos(self.basic_histos,'onlTauMu',tau)
+        ## check whether Tau has leading track online - MuVtx
+        if onl_tau_mu_vtx.tauID('decayModeFinding') < 0.5  :
           self.failinDMHLT_mu    += 1
-          passMu = False
-          self.fillBasicHistos(self.basic_histos,'onlTauMuPassingDM',event.tau)
+          if tau.onlineLeadingTrack is not False :  
+            self.fillTrackHistos(self.track_histos, 'offTrk_failDMonl_hasOnlTrk_mu', tau.offlineLeadingTrack, offVtx[0], onlPixVtx[0], onlMuVtx[0])
+            self.fillTrackHistos(self.track_histos, 'onlTrk_failDMonl_hasOnlTrk_mu', tau.onlineLeadingTrack , offVtx[0], onlPixVtx[0], onlMuVtx[0])
+          else :
+            self.fillTrackHistos(self.track_histos, 'offTrk_failDMonl_noOnlTrk_mu' , tau.offlineLeadingTrack, offVtx[0], onlPixVtx[0], onlMuVtx[0])
+        else :
+          self.fillBasicHistos(self.basic_histos, 'onlTauMuPassingDM',tau) 
+        ## check whether Tau is isolated online - MuVtx
+        if onl_tau_mu_vtx.tauID('byIsolation') < 0.5 : self.failinIsoHLT_mu   += 1
+        else                                         :  self.fillBasicHistos(self.basic_histos,'onlTauMuPassingIso',tau)
 
-      if passPix :
-        if event.onl_tau_pix_vtx.tauID('decayModeFinding') < 0.5 :
+      ## check whether Tau is reconstructed online - PixVtx
+      if not onl_tau_pix_vtx :
+        self.failinRecoHLT_pix += 1  
+      else :
+        self.fillBasicHistos(self.basic_histos,'onlTauPix',tau)
+        ## check whether Tau has leading track online - PixVtx      
+        if onl_tau_pix_vtx.tauID('decayModeFinding') < 0.5 :
           self.failinDMHLT_pix   += 1
-          passPix = False
-          self.fillBasicHistos(self.basic_histos,'onlTauPixPassingDM',event.tau)
-
-      if passMu :
-        if event.onl_tau_mu_vtx.tauID('byIsolation') < 0.5 :
-          self.failinIsoHLT_mu   += 1
-          passMu = False
-          self.fillBasicHistos(self.basic_histos,'onlTauMuPassingIso',event.tau)
-
-      if passPix :
-        if event.onl_tau_pix_vtx.tauID('byIsolation') < 0.5 :
-          self.failinIsoHLT_pix  += 1        
-          passPix = False
-          self.fillBasicHistos(self.basic_histos,'onlTauPixPassingIso',event.tau)
-      
-      #import pdb ; pdb.set_trace()
+          if tau.onlineLeadingTrack is not False :  
+            self.fillTrackHistos(self.track_histos, 'offTrk_failDMonl_hasOnlTrk_pix', tau.offlineLeadingTrack, offVtx[0], onlPixVtx[0], onlMuVtx[0])
+            self.fillTrackHistos(self.track_histos, 'onlTrk_failDMonl_hasOnlTrk_pix', tau.onlineLeadingTrack , offVtx[0], onlPixVtx[0], onlMuVtx[0])
+          else :
+            self.fillTrackHistos(self.track_histos, 'offTrk_failDMonl_noOnlTrk_pix', tau.offlineLeadingTrack, offVtx[0], onlPixVtx[0], onlMuVtx[0])
+        else :  
+          self.fillBasicHistos(self.basic_histos, 'onlTauPixPassingDM',tau)  
+        ## check whether Tau is isolated online - PixVtx
+        if onl_tau_pix_vtx.tauID('byIsolation') < 0.5 : self.failinIsoHLT_pix  += 1        
+        else                                          : self.fillBasicHistos(self.basic_histos,'onlTauPixPassingIso',tau)
       
   def produceCollections(self, event, handles) :
     for key in handles.keys() :
@@ -136,6 +128,7 @@ class PATreader() :
     self.handles[ 'offVtx'         ] = [ Handle('std::vector<reco::Vertex>'     ),'selectedPrimaryVertices'          ]
       
     ## online
+    #self.handles[ 'onlTausPixVtx2' ] = [ Handle('std::vector<pat::Tau>'         ),'selectedHltPatTausOnl2NP'         ] ## new features by Michal, to be checked
     self.handles[ 'onlTausMuVtx'   ] = [ Handle('std::vector<pat::Tau>'         ),'selectedHltPatTaus'               ]
     self.handles[ 'onlTausPixVtx'  ] = [ Handle('std::vector<pat::Tau>'         ),'selectedHltPatTausStdVtx'         ]
     self.handles[ 'onlPFcandidates'] = [ Handle('std::vector<reco::PFCandidate>'),'hltParticleFlowForTaus'           ]
@@ -177,13 +170,36 @@ class PATreader() :
            tau.tauID('againstMuonTight')                         > 0.5  and \
            tau.tauID('againstElectronLoose')                     > 0.5  
 
+  def deltaPhi(self, phi1, phi2):
+    PHI = abs(phi1-phi2)
+    if (PHI<=math.pi):
+      return PHI
+    else:
+      return 2*math.pi-PHI
+
+  def checkLeadingTrack(self, tau) :
+    if tau.leadPFChargedHadrCand().trackRef().isNull() :
+      print 'event: ',self.event.eventAuxiliary().event(),'\t good offline Tau leading track has invalid leadPFChargedHadrCand().trackRef()\t SKIPPING' 
+      return False
+    else :
+      return tau.leadPFChargedHadrCand().trackRef()
+
+  def deltaR(self, p1, p2) :
+    eta1 = p1.eta()
+    phi1 = p1.phi()
+    eta2 = p2.eta()
+    phi2 = p2.phi()
+    deta = eta1-eta2
+    dphi = self.deltaPhi(phi1,phi2)
+    return math.sqrt(deta*deta + dphi*dphi)
+
   def best_matching(self, coll1, coll2, dR=0.5) :
     association = {}
     for p1 in coll1 :
       tmp = {p1:False}
       for p2 in coll2 :
-        if deltaR(p1,p2) < dR :
-          dR = deltaR(p1,p2)
+        if self.deltaR(p1,p2) < dR :
+          dR = self.deltaR(p1,p2)
           tmp = {p1:p2}
       association.update(tmp)
     return association        
@@ -259,7 +275,7 @@ class PATreader() :
     try    : histos[name]['genDM' ].Fill(particle.genDM    )
     except : pass
     
-  def fillTrackHistos(self, histos, name, particle, offvtx, hltpixvtx, hltmuvtx) :
+  def fillTrackHistos(self, histos, name, track, offvtx, hltpixvtx, hltmuvtx) :
     try    : histos[name]['pt'                        ] .Fill(track.pt()                                     )
     except : pass
     try    : histos[name]['errPtOverPt'               ] .Fill(track.ptError()/track.pt()                     )
@@ -286,17 +302,17 @@ class PATreader() :
     except : pass
     try    : histos[name]['numberOfValidTrackerHits'  ] .Fill(track.hitPattern().numberOfValidTrackerHits()  )
     except : pass
-    try    : histos[name]['dxy(offlineVtx)'           ] .Fill(track.dxy(offvtx)                              )
+    try    : histos[name]['dxy(offlineVtx)'           ] .Fill(track.dxy(offvtx.position())                   )
     except : pass
-    try    : histos[name]['dz(offlineVtx)'            ] .Fill(track.dz(offvtx)                               )
+    try    : histos[name]['dz(offlineVtx)'            ] .Fill(track.dz(offvtx.position())                    )
     except : pass
-    try    : histos[name]['dxy(hltPixVtx)'            ] .Fill(track.dxy(hltpixvtx)                           )
+    try    : histos[name]['dxy(hltPixVtx)'            ] .Fill(track.dxy(hltpixvtx.position())                )
     except : pass
-    try    : histos[name]['dz(hltPixVtx)'             ] .Fill(track.dz(hltpixvtx)                            )
+    try    : histos[name]['dz(hltPixVtx)'             ] .Fill(track.dz(hltpixvtx.position())                 )
     except : pass
-    try    : histos[name]['dxy(hltMuVtx)'             ] .Fill(track.dxy(hltmuvtx)                            )
+    try    : histos[name]['dxy(hltMuVtx)'             ] .Fill(track.dxy(hltmuvtx.position())                 )
     except : pass
-    try    : histos[name]['dz(hltMuVtx)'              ] .Fill(track.dz(hltmuvtx)                             )
+    try    : histos[name]['dz(hltMuVtx)'              ] .Fill(track.dz(hltmuvtx.position())                  )
     except : pass
     try    : histos[name]['dRoffline'                 ] .Fill(deltaR(track,offTau)                           )
     except : pass
--- working/runner.py
+++ working/runner.py
@@ -1,7 +1,9 @@
 import ROOT
 import PATreader
 from array import array
+from files import *
 
+# bins = array('d',[0.,17.5,20.,22.5,25.,30.,40.,50.,60.,80.,120.,250.,500,])
 bins = array('d',[0.,17.5,20.,22.5,25.,30.,40.,50.,60.,80.,120.])
 
 basic_histos = {}
@@ -17,42 +19,69 @@ for name in names :
   basic_histos[name].update({'genDM'  : ROOT.TH1F( name+'_genDM'  , '' , 25, -2.5, 2.5    )} )
 
 track_histos = {}
-names  = ['offTrk_pass','offTrk_fail']
-
+names  = ['offTrk_failDMonl_hasOnlTrk_mu','onlTrk_failDMonl_hasOnlTrk_mu','offTrk_failDMonl_noOnlTrk_mu' ,'offTrk_failDMonl_hasOnlTrk_pix','onlTrk_failDMonl_hasOnlTrk_pix','offTrk_failDMonl_noOnlTrk_pix']
+ 
 for name in names :
   track_histos.update({name:{}})
-  track_histos[name].update({'pt'                         : ROOT.TH1F( name+'_pt'                        , '' , len(bins)-1, bins)} )
-  track_histos[name].update({'errPtOverPt'                : ROOT.TH1F( name+'_errPtOverPt'               , '' , 20, 0   , 100    )} )
-  track_histos[name].update({'eta'                        : ROOT.TH1F( name+'_eta'                       , '' , 25, -2.5, 2.5    )} )
-  track_histos[name].update({'phi'                        : ROOT.TH1F( name+'_phi'                       , '' , 25, -2.5, 2.5    )} )
-  track_histos[name].update({'charge'                     : ROOT.TH1F( name+'_charge'                    , '' , 5 , -2  , 2      )} )
-  track_histos[name].update({'chi2'                       : ROOT.TH1F( name+'_chi2'                      , '' , 40, 0   , 20     )} )
-  track_histos[name].update({'ndof'                       : ROOT.TH1F( name+'_ndof'                      , '' , 40, 0   , 40     )} )
-  track_histos[name].update({'normalizedChi2'             : ROOT.TH1F( name+'_normalizedChi2'            , '' , 20, 0   , 20     )} )
-  track_histos[name].update({'numberOfLostHits'           : ROOT.TH1F( name+'_numberOfLostHits'          , '' , 20, 0   , 20     )} )
-  track_histos[name].update({'numberOfValidHits'          : ROOT.TH1F( name+'_numberOfValidHits'         , '' , 20, 0   , 20     )} )
-  track_histos[name].update({'highPurity'                 : ROOT.TH1F( name+'_highPurity'                , '' , 5 , 0   , 5      )} )
-  track_histos[name].update({'numberOfValidPixelHits'     : ROOT.TH1F( name+'_numberOfValidPixelHits'    , '' , 20, 0   , 20     )} )
-  track_histos[name].update({'pixelLayersWithMeasurement' : ROOT.TH1F( name+'_pixelLayersWithMeasurement', '' , 20, 0   , 20     )} )
-  track_histos[name].update({'numberOfValidTrackerHits'   : ROOT.TH1F( name+'_numberOfValidTrackerHits'  , '' , 20, 0   , 20     )} )
-  track_histos[name].update({'dxy(offlineVtx)'            : ROOT.TH1F( name+'_dxy(offlineVtx)'           , '' , 40, 0   , 2      )} )
-  track_histos[name].update({'dz(offlineVtx)'             : ROOT.TH1F( name+'_dz(offlineVtx)'            , '' , 40, 0   , 20     )} )
-  track_histos[name].update({'dxy(hltPixVtx)'             : ROOT.TH1F( name+'_dxy(hltPixVtx)'            , '' , 40, 0   , 2      )} )
-  track_histos[name].update({'dz(hltPixVtx)'              : ROOT.TH1F( name+'_dz(hltPixVtx)'             , '' , 40, 0   , 20     )} )
-  track_histos[name].update({'dxy(hltMuVtx)'              : ROOT.TH1F( name+'_dxy(hltMuVtx)'             , '' , 40, 0   , 2      )} )
-  track_histos[name].update({'dz(hltMuVtx)'               : ROOT.TH1F( name+'_dz(hltMuVtx)'              , '' , 40, 0   , 20     )} )
-  track_histos[name].update({'algo'                       : ROOT.TH1F( name+'_algo'                      , '' , 8 , 0   , 8      )} )
-  track_histos[name].update({'dRoffline'                  : ROOT.TH1F( name+'_dRoffline'                 , '' , 40, 0   , 4      )} )
-  track_histos[name].update({'dRonline'                   : ROOT.TH1F( name+'_dRonline'                  , '' , 40, 0   , 4      )} )
+  track_histos[name].update({'pt'                         : ROOT.TH1F( name+'_pt'                        , '' , 24, 0   , 120 )} )
+  track_histos[name].update({'errPtOverPt'                : ROOT.TH1F( name+'_errPtOverPt'               , '' , 20, 0   , 100 )} )
+  track_histos[name].update({'eta'                        : ROOT.TH1F( name+'_eta'                       , '' , 25, -2.5, 2.5 )} )
+  track_histos[name].update({'phi'                        : ROOT.TH1F( name+'_phi'                       , '' , 25, -2.5, 2.5 )} )
+  track_histos[name].update({'charge'                     : ROOT.TH1F( name+'_charge'                    , '' , 5 , -2  , 2   )} )
+  track_histos[name].update({'chi2'                       : ROOT.TH1F( name+'_chi2'                      , '' , 40, 0   , 20  )} )
+  track_histos[name].update({'ndof'                       : ROOT.TH1F( name+'_ndof'                      , '' , 40, 0   , 40  )} )
+  track_histos[name].update({'normalizedChi2'             : ROOT.TH1F( name+'_normalizedChi2'            , '' , 20, 0   , 20  )} )
+  track_histos[name].update({'numberOfLostHits'           : ROOT.TH1F( name+'_numberOfLostHits'          , '' , 20, 0   , 20  )} )
+  track_histos[name].update({'numberOfValidHits'          : ROOT.TH1F( name+'_numberOfValidHits'         , '' , 20, 0   , 20  )} )
+  track_histos[name].update({'highPurity'                 : ROOT.TH1F( name+'_highPurity'                , '' , 5 , 0   , 5   )} )
+  track_histos[name].update({'numberOfValidPixelHits'     : ROOT.TH1F( name+'_numberOfValidPixelHits'    , '' , 20, 0   , 20  )} )
+  track_histos[name].update({'pixelLayersWithMeasurement' : ROOT.TH1F( name+'_pixelLayersWithMeasurement', '' , 20, 0   , 20  )} )
+  track_histos[name].update({'numberOfValidTrackerHits'   : ROOT.TH1F( name+'_numberOfValidTrackerHits'  , '' , 20, 0   , 20  )} )
+  track_histos[name].update({'dxy(offlineVtx)'            : ROOT.TH1F( name+'_dxy(offlineVtx)'           , '' , 40, 0   , 2   )} )
+  track_histos[name].update({'dz(offlineVtx)'             : ROOT.TH1F( name+'_dz(offlineVtx)'            , '' , 40, 0   , 20  )} )
+  track_histos[name].update({'dxy(hltPixVtx)'             : ROOT.TH1F( name+'_dxy(hltPixVtx)'            , '' , 40, 0   , 2   )} )
+  track_histos[name].update({'dz(hltPixVtx)'              : ROOT.TH1F( name+'_dz(hltPixVtx)'             , '' , 40, 0   , 20  )} )
+  track_histos[name].update({'dxy(hltMuVtx)'              : ROOT.TH1F( name+'_dxy(hltMuVtx)'             , '' , 40, 0   , 2   )} )
+  track_histos[name].update({'dz(hltMuVtx)'               : ROOT.TH1F( name+'_dz(hltMuVtx)'              , '' , 40, 0   , 20  )} )
+  track_histos[name].update({'algo'                       : ROOT.TH1F( name+'_algo'                      , '' , 8 , 0   , 8   )} )
+  track_histos[name].update({'dRoffline'                  : ROOT.TH1F( name+'_dRoffline'                 , '' , 40, 0   , 4   )} )
+  track_histos[name].update({'dRonline'                   : ROOT.TH1F( name+'_dRonline'                  , '' , 40, 0   , 4   )} )
 
+# myFiles = ['/afs/cern.ch/work/m/manzoni/TauHLT/CMSSW_5_3_14_patch2_trg/src/TriggerStudies/Tau/test/patTuple.root',] 
+# myFiles = ['/afs/cern.ch/work/m/manzoni/TauHLT/CMSSW_5_3_14_patch2_trg/src/TriggerStudies/Tau/test/patTuple_loosenIter0Jet_v2.root',]  
+# myFiles = ['/afs/cern.ch/work/m/manzoni/TauHLT/CMSSW_5_3_14_patch2_trg/src/TriggerStudies/Tau/test/patTuple_originalIter0Jet_v2.root',] 
+failingEvents = [28150788,38979698,9377365,34287744,2073115,62411153,61730373,45685986,47009379,4002807,22405779,56029277,18718809,52991929,13991388,1953725,16196942,67886619,16228470,16138268,16277738,27435007,9365765,22024092,28389058,27226213,27228342,30051102,19575720,62078292,3835767,21848492,2682170,11037065,16270377,26689779,24705865,38498563,40177109,6063353,3924919,61479375,24710730,38972916,38951854,56561840,61830800,19760180,16253201,36230639,33444947,12782522,45053556,4441121,66238069,65020368,24505983,11490191,12838373,14199880,53197377,1382277,12812297,1249504,21513095,25618944,16215296,37977062,68700985,40605935,61890250,13727268,65101560,38184411,9353868,38410378,16160789,16273551,63543902,26934110,9368795,37232492,11498432,52813775,52536300,70645969,6524418,9355742,16189544,16190949]  
 
-myAnalyzer = PATreader.PATreader(basic_histos=basic_histos, track_histos=track_histos)
-myAnalyzer.looper(maxEvents=1000, pickSingleEvent=-1, pickSomeEvents=[], verbose=False)
+myAnalyzer = PATreader.PATreader(myFiles, basic_histos=basic_histos, track_histos=track_histos)
+myAnalyzer.looper(maxEvents=-1, pickEvents=[], verbose=False)
 myAnalyzer.printSummary()
-
-# failingEvents = [28150788,38979698,9377365,34287744,2073115,62411153,61730373,45685986,47009379,4002807,22405779,56029277,18718809,52991929,13991388,1953725,16196942,67886619,16228470,16138268,16277738,27435007,9365765,22024092,28389058,27226213,27228342,30051102,19575720,62078292,3835767,21848492,2682170,11037065,16270377,26689779,24705865,38498563,40177109,6063353,3924919,61479375,24710730,38972916,38951854,56561840,61830800,19760180,16253201,36230639,33444947,12782522,45053556,4441121,66238069,65020368,24505983,11490191,12838373,14199880,53197377,1382277,12812297,1249504,21513095,25618944,16215296,37977062,68700985,40605935,61890250,13727268,65101560,38184411,9353868,38410378,16160789,16273551,63543902,26934110,9368795,37232492,11498432,52813775,52536300,70645969,6524418,9355742,16189544,16190949]  
 
 myAnalyzer.returnBasicHistos()
 myAnalyzer.returnTrackHistos()
 
-import pdb ; pdb.set_trace()
+outFile = ROOT.TFile.Open('out2.root','recreate')
+
+outFile.cd()
+outFile.mkdir('taus')
+outFile.cd('taus')
+
+for key in basic_histos.keys() :
+  outFile.mkdir(key)
+  outFile.cd(key)
+  for histo in basic_histos[key].values() :
+    histo.Write()
+  outFile.cd('taus')
+
+outFile.cd()
+outFile.mkdir('tracks')
+outFile.cd('tracks')
+
+for key in track_histos.keys() :
+  outFile.mkdir(key)
+  outFile.cd(key)
+  for histo in track_histos[key].values() :
+    histo.Write()
+  outFile.cd('tracks')
+
+outFile.Close()
+
